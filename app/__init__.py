59fd4f6781ee1e3afa05bd0c4793efa289e15124
phuman1872002@gmail.com <phuman1872002@gmail.com>
2024-08-05 11:30:09 +0700

    thay lai cau truc va model

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from .configs import Config
+from urllib.parse import quote
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/ebookdb?charset=utf8mb4" % quote(
+    "123456")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
 
 db = SQLAlchemy(app=app)
-migrate = Migrate(app=app, db=db)
 
-with app.app_context():
-    from .models import Category
+login = LoginManager(app=app)
+
